bd99909faebaa5ec0d5a2bd310fe4b46ce75a5c5
Jack-LP <jackpaget1@gmail.com>
2023-02-12 23:21:55 +0000

    Added tailwindCSS

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        charcoal: {
+          100: '#35383f',
+          200: '#2c2d31',
+          300: '#1e1f23',
+          400: '#15161a',
+        },
+      },
+    },
+  },
+  plugins: [],
+};

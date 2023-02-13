3a4edc97485b6a315b4f16f7e46df07cb7aa2cf5
Jack-LP <jackpaget1@gmail.com>
2023-02-13 19:37:02 +0000

    Added file download options

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,27 @@
-import React, { useContext, useState } from 'react';
+import React, { useState } from 'react';
 import DocumentContext from '../../context/DocumentContext';
-import { List, Save, FileEarmark } from 'react-bootstrap-icons';
+import {
+  List,
+  ChevronDown,
+  ChevronUp,
+  FileEarmark,
+} from 'react-bootstrap-icons';
 
 const Navbar = ({ input, setTrayOpen, docTitle, setDocTitle }) => {
-  const saveDoc = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const saveDoc = (format) => {
     const link = document.createElement('a');
     const file = new Blob([input], { type: 'text/plain' });
     link.href = URL.createObjectURL(file);
-    link.download = `${docTitle}.md`;
+    link.download = `${docTitle}.${format}`;
     link.click();
     URL.revokeObjectURL(link.href);
+    setMenuOpen(false);
   };
 
   return (
-    <nav className='flex items-center justify-between gap-5 bg-charcoal-200'>
+    <nav className='flex items-center justify-between gap-5 bg-charcoal-200 pr-5'>
       <div className='flex items-center gap-5'>
         <button
           onClick={() => setTrayOpen((prev) => !prev)}
@@ -37,13 +45,34 @@ const Navbar = ({ input, setTrayOpen, docTitle, setDocTitle }) => {
           </div>
         </div>
       </div>
-      <button
-        onClick={saveDoc}
-        className='bg-myOrange py-2 px-3 rounded-md flex gap-2 items-center mr-5'
-      >
-        <Save />
-        Save
-      </button>
+      <div className='relative'>
+        <button
+          onClick={() => setMenuOpen((prev) => !prev)}
+          className='bg-myOrange py-2 px-5 rounded-md flex gap-2 items-center'
+        >
+          {menuOpen ? <ChevronUp /> : <ChevronDown />}
+          Save As...
+        </button>
+        <div
+          className={`${
+            menuOpen ? 'flex' : 'hidden'
+          } flex-col gap-2 absolute top-12 bg-charcoal-100 w-full p-2 rounded-md`}
+        >
+          <button
+            onClick={() => saveDoc('txt')}
+            className='border-[1px] border-white rounded-md py-1 z-10'
+          >
+            .txt
+          </button>
+          <button
+            onClick={() => saveDoc('md')}
+            className='border-[1px] border-white rounded-md py-1 z-10'
+          >
+            .md
+          </button>
+          <div className='absolute w-4 h-4 bg-charcoal-100 rotate-45 top-[3px] left-1/2 transform -translate-x-1/2 -translate-y-1/2'></div>
+        </div>
+      </div>
     </nav>
   );
 };

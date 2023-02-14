59d9d6921e2f93eba9334a7a4fd1db5677aaccd4
Jack-LP <jackpaget1@gmail.com>
2023-02-14 14:30:11 +0000

    Cleaned css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { setToStorage, getFromStorage } from '../utils/localStorage';
 import Navbar from './components/Navbar';
 import TextSection from './components/TextSection';
 import SideTray from './components/SideTray';
 
 const App = () => {
-  const [input, setInput] = useState('');
-  const [docTitle, setDocTitle] = useState('untitled-document');
+  const [input, setInput] = useState(getFromStorage('localDoc', true) || '');
+  const [docTitle, setDocTitle] = useState('untitled');
   const [trayOpen, setTrayOpen] = useState();
+
+  useEffect(() => {
+    fetch('/data/welcome.txt')
+      .then((r) => r.text())
+      .then((welcome) => {
+        if (input === '') {
+          setInput(welcome);
+          console.log(welcome);
+        }
+      });
+  }, []);
+
+  useEffect(() => {
+    setToStorage('localDoc', input);
+  }, [input]);
 
   return (
     <div className='w-screen h-screen bg-charcoal-400 text-white font-commissioner caret-myOrange'>
